5dc2febbc693d2e983ce8a4ee1665b47ff7986b4
Danilo Pimentel <danilo.pimentel97@gmail.com>
2017-04-17 10:04:29 -0300

    Added list header functions and queue header.

--- include/list.h
+++ include/list.h
@@ -1,4 +1,42 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * @brief      Cell structure
+ * 
+ * This struct is used on the list as cell.
+ */
+struct cell {
+    /** Next cell on the list */
+    struct cell *next;
+    /** Stored data */
+    int data;
+};
+
+typedef struct cell Cell;
+
+/**
+ * @brief      List structure
+ * 
+ * This will be the structure of the list.
+ */
+struct list {
+    /** Pointer to the first item on the list */
+    Cell *first;
+    /** Size of the list */
+    int size;
+};
+
+typedef struct list List;
+
+void initializeList(List *list);
+
+void initializeCell(Cell *cell);
+
+void insertCell(List *list, Cell *cell, int position);
+
+void removeCell(List *list, int position);
+
+void printList(List list);
+
 #endif
--- src/list.c
+++ src/list.c
@@ -1,1 +1,206 @@
 #include "list.h"
+#include <stdio.h>
+
+/**
+ * @brief      Initialize a List
+ * 
+ * This function initialize a empty list. The first item and size is set to NULL and 0, respectively.
+ *
+ * @param      list  The list that will be initialized.
+ */
+void initializeList(List *list) {
+    list->first = NULL;
+    list->size = 0;
+}
+
+/**
+ * @brief      Initialize a Cell
+ * 
+ * This funciton will initialize a new empty cell. The next item will be set to NULL and the stored data will be set to 0.
+ *
+ * @param      cell  The cell that will be initialized.
+ */
+void initializeCell(Cell *cell) {
+    cell->next = NULL;
+    cell->data = 0;
+}
+
+/**
+ * @brief      Calculates the potition.
+ * 
+ * Calculates the position of something based on the size.
+ *
+ * @param[in]  position  The position
+ * @param[in]  size      The size
+ *
+ * @return     The calculated position.
+ */
+int calculatePotition(int position, int size) {
+    // If the position is negative, it will be interpreted that the cell have to
+    // be inserted on the x position before the end of the list.
+    if (position < 0) {
+        position = size + position;
+    }
+
+    // If the given position is greater than the size of the list, it will be
+    // reassigned to the end of the list.
+    if(size < position) {
+        position = size;
+    }
+
+    return position;
+}
+
+/**
+ * @brief      Insert a Cell
+ * 
+ * This function will insert a cell in a determined position of the given list.
+ *
+ * @param      list      List to insert the given cell
+ * @param      cell      The cell
+ * @param[in]  position  Position that the cell will be inserted
+ */
+void insertCell(List *list, Cell *cell, int position) {
+    int counter = 0;
+    Cell *previous = NULL, *current = NULL;
+
+    position = calculatePotition(position, list->size);
+
+    current = list->first;
+
+
+    // This loop will search the given position over the list iterating on the
+    // cells. Once it arrive to the position, the "current" pointer will be
+    // pointing to the cell that is on the given position.
+    while (counter < position) {
+        previous = current;
+        current = current->next;
+        counter++;
+    }
+
+    // There are 3 cases to be treated. The first, last or middle position.
+
+    // First position: current will point to the first cell and previous will be
+    // NULL. list->first will be set to the new cell and if the list size is
+    // greater than 0, the cell->next have to point to the old first cell.
+
+    // Last position: current will be NULL and previous to the last element.
+    // previous->next have to be set to cell. As cell->next is NULL by default,
+    // nothing will be done.
+
+    // Middle position: current and previous will be pointing to cells on the
+    // list. previous->next have to point to cell. cell->next have to point to 
+    // current.
+
+
+    // First position
+    if(previous == NULL) {
+        list->first = cell;
+
+        // If the list is empty, current will be NULL. So assign it to
+        // cell->next still working
+        cell->next = current;
+    }
+    // Last position
+    else if(previous != NULL && current == NULL) {
+        previous->next = cell;
+    }
+    // The list has 2 or more items
+    else {
+        previous->next = cell;
+        cell->next = current;
+    }
+
+    list->size++;
+}
+
+/**
+ * @brief      Removes a Cell
+ * 
+ * This function will remove a cell in a determined position of the given list.
+ *
+ * @param      list      The list
+ * @param[in]  position  The position of the cell to be removed
+ */
+void removeCell(List *list, int position) {
+    int counter = 0;
+    Cell *previous = NULL, *current = NULL;
+
+    position = calculatePotition(position, list->size);
+
+    // If the position is equal to the size, it will be trying to remove a cell
+    // that dont exist. With that, we can presume that the cell to be removed
+    // is the last.
+    if(position == list->size) {
+        position = list->size - 1;
+    }
+
+    current = list->first;
+
+
+    // This loop will search the given position over the list iterating on the
+    // cells. Once it arrive to the position, the "current" pointer will be
+    // pointing to the cell that is on the given position.
+    while (counter < position) {
+        previous = current;
+        current = current->next;
+        counter++;
+    }
+
+    // There are 3 cases to be treated. The first, last or middle position.
+
+    // First position: current will point to the first cell and previous will be
+    // NULL. list->first will be set to the current->next.
+
+    // Last position: The difference of the last position to the insertCell
+    // function is that here, if the size of the list is x, you cant remove the
+    // cell that is on the position x. On insert funciton, it would be a valid
+    // position, because it would sign that the cell have to be the last cell.
+    // Here, last position means the x-1 position. In this case, current will 
+    // be pointing to the last cell of the list. With that, current->next will
+    // be NULL. previous->next have to be set to NULL.
+
+    // Middle position: current and previous will be pointing to cells on the
+    // list. previous->next have to point to current->next.
+
+
+    // First position
+    if(previous == NULL) {
+        // If the list is empty, current will be NULL. If is that the case, 
+        // the list have no cells to be removed and nothing have to be done.
+        if(current != NULL)
+            list->first = current->next;
+    }
+    // Last position
+    else if(previous != NULL && current != NULL && current->next == NULL) {
+        previous->next = NULL;
+    }
+    // The list has 2 or more items
+    else {
+        previous->next = current->next;
+    }
+
+    // If the list is empty, there is no reason to decrease the size, because
+    // any cell was removed.
+    if(list->size > 0) {
+        list->size--;
+    }
+}
+
+/**
+ * @brief      Print a List
+ * 
+ * This function will print the content of a list. It prints the position and the data of each cell.
+ *
+ * @param[in]  list  The list
+ */
+void printList(List list) {
+    int counter = 0;
+    Cell *current = list.first;
+
+    while (counter < list.size) {
+        printf("%d - %d\n", counter, current->data);
+        current = current->next;
+        counter++;
+    }
+}
